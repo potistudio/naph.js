4c932abbdd12dc844ea93a682dd60fac41b5776e
POTI <potistudio2022@gmail.com>
2023-12-09 23:57:41 +0900

    Import JUCE Library in C++

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,10 @@
 
+#define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED true
+#define DONT_SET_USING_JUCE_NAMESPACE true
+
 #include <iostream>
 #include <napi.h>
+#include "juce_core.h"
 
 class PluginHostWrapper : public Napi::ObjectWrap<PluginHostWrapper> {
 	public:
