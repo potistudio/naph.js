dd33ecf9d4da04c6fd8a260d7689b170d8fb0571
POTI <potistudio2022@gmail.com>
2023-12-10 22:46:12 +0900

    Cleanup

--- test/test.js
+++ test/test.js
@@ -5,9 +5,3 @@ console.log (naph.hello()); // "Hello World!"
 console.log (naph.PluginHost.helloStatic()); // "Hello from Static Method!"
 const host = new naph.PluginHost(); // "Hello from Constructor!"
 console.log (host.helloInstance()); // "Hello from Instance Method!"
-
-!async function main() {
-	host.showAlert (() => {
-		console.log("Main");
-	});
-}();
