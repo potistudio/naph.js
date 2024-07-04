0b3b96ee60ff197d275cca036ea219bb49a21a14
POTI <potistudio2022@gmail.com>
2024-07-05 05:35:35 +0900

    🎉 feat: Load VST3

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <napi.h>
 #include "juce_core/juce_core.h"
-#include "juce_gui_basics/juce_gui_basics.h"
+#include "juce_audio_processors/juce_audio_processors.h"
 
 class PluginHostWrapper : public Napi::ObjectWrap<PluginHostWrapper> {
 	public:
 		static Napi::Object Init (Napi::Env env, Napi::Object exports) {
 			Napi::Function func = DefineClass (env, "PluginHostWrapper", {
 				InstanceMethod ("helloInstance", &PluginHostWrapper::HelloInstance, napi_enumerable),
+				InstanceMethod ("createVST3Instance", &PluginHostWrapper::CreateVST3Instance, napi_enumerable),
 				StaticMethod ("helloStatic", &PluginHostWrapper::HelloStatic, napi_enumerable)
 			});
 			Napi::FunctionReference *constructor = new Napi::FunctionReference();
@@ -38,6 +39,19 @@ class PluginHostWrapper : public Napi::ObjectWrap<PluginHostWrapper> {
 
 		Napi::Value HelloInstance (const Napi::CallbackInfo& info) {
 			return Napi::String::New (info.Env(), "Hello from Instance Method!");
+		}
+
+		void CreateVST3Instance (const Napi::CallbackInfo& info) {
+			juce::OwnedArray<juce::PluginDescription> descriptions;
+
+			juce::VST3PluginFormat format;
+			juce::StringArray foundPlugins = format.searchPathsForPlugins (juce::FileSearchPath("C:/Program Files/Common Files/VST3"), false, true);
+			format.findAllTypesForFile (descriptions, foundPlugins[0]);
+
+			juce::PluginDescription* description = descriptions[0];
+			std::unique_ptr<juce::AudioPluginInstance> instance = format.createInstanceFromDescription (*description, 48000, 1024);
+
+			std::cout << description->name << std::endl;
 		}
 };
 
--- test/test.js
+++ test/test.js
@@ -5,3 +5,5 @@ console.log (naph.hello()); // "Hello World!"
 console.log (naph.PluginHost.helloStatic()); // "Hello from Static Method!"
 const host = new naph.PluginHost(); // "Hello from Constructor!"
 console.log (host.helloInstance()); // "Hello from Instance Method!"
+
+host.createVST3Instance();
